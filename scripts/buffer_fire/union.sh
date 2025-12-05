7557c3019e4420ecae8ffc997b429c958e1db1c5
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2025-12-05 12:23:13 -0800

    Update union.sh

--- scripts/buffer_fire/union.sh
+++ scripts/buffer_fire/union.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-# 10, 40, 100
-
 for year in $(seq 2024 -1 1984); do
     echo "Computing fire_area_canada_usa$year.geometry union …"
-
-    # Script 1: compute lakes with overlap
+    
     sed "s/{{YEAR}}/$year/g" union.sql | psql -d spatial
 
     echo "Completed year $year."

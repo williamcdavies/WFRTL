ef3beef550e4d7e8b0bee1be4be0e312b16aadcb
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2025-12-05 15:49:39 -0800

    Modify echo statements for readability and clarity

--- scripts/buffer_fire/union.sh
+++ scripts/buffer_fire/union.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 for year in $(seq 2024 -1 1984); do
-    echo "Computing fire_area_canada_usa$year.geometry union …"
+    echo "Computing and inserting union for year $year …"
     
     sed "s/{{YEAR}}/$year/g" union.sql | psql -d spatial
 
-    echo "Completed year $year."
+    echo "Done."
 done

e6e9856f3df8c9c2dcaa948117bc0795917e302c
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2025-12-12 11:52:38 -0800

    Update buffer.sh

--- scripts/buffer_fire/buffer.sh
+++ scripts/buffer_fire/buffer.sh
@@ -8,6 +8,14 @@
 
 echo "Updating fire_area_canada_usa with buffer of distance $1 m …"
 
-sed "s/{{BUFFER_DISTANCE}}/$1/g" buffer.sql | psql -d spatial
-   
-echo "Done."
+sed "s/{{BUFFER_DISTANCE}}/$1/g" buffer_parent.sql | psql -d spatial
+
+for year in $(seq 2024 -1 1984); do
+    echo "Updating fire_area_canada_usa$year with buffer of distance $1 m …"
+
+    sed -e "s/{{BUFFER_DISTANCE}}/$1/g" \
+        -e "s/{{YEAR}}/$year/g" \
+        buffer_children.sql | psql -d spatial
+    
+    echo "Done."
+done

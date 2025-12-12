9163488dd651fa9636b3dc048a3fb0295a11cec1
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2025-12-12 11:52:49 -0800

    Update union.sh

--- scripts/buffer_fire/union.sh
+++ scripts/buffer_fire/union.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# Usage: `sh union.sh <buffer_distance>`
+
 for year in $(seq 2024 -1 1984); do
     echo "Computing and inserting union for year $year …"
     
-    sed "s/{{YEAR}}/$year/g" union.sql | psql -d spatial
+    sed -e "s/{{BUFFER_DISTANCE}}/$1/g" \
+        -e "s/{{YEAR}}/$year/g" \
+        union.sql | psql -d spatial
 
     echo "Done."
 done

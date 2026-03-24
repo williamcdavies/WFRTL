49a9c84fd0ef28eeec10ce9f07f26b1618c4e543
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-03-23 19:52:08 -0700

    Update q.sql

--- scripts/fire_union_over_watersheds_polys/with_lakes/q.sql
+++ scripts/fire_union_over_watersheds_polys/with_lakes/q.sql
@@ -8,34 +8,24 @@ CREATE TEMP TABLE fire_area_canada_usa_composite{{YEAR}} (
     overlap            boolean
 );
 
-\copy fire_area_canada_usa_composite{{YEAR}} FROM '/Users/williamchuter-davies/Downloads/UNR WF FIRE_UNION_POLY_OVER_WATERSHEDS_POLYS_DATA-selected/fire_area_canada_usa_composite{{YEAR}}.csv' CSV HEADER;
+\copy fire_area_canada_usa_composite{{YEAR}} FROM '/Users/williamchuter-davies/Documents/Projects/P-10/WFRTL/scripts/fire_union_over_watersheds_polys/with_lakes/UNR WF FIRE_UNION_POLY_OVER_WATERSHEDS_POLYS_DATA-selected/fire_area_canada_usa_composite{{YEAR}}.csv' CSV HEADER;
 
 COPY (
     SELECT
-        x.id,
-        x.tnmid,
-        x.globalid,
-        x.huc12,
-        x.name,
-        x.overlap_percentage,
-        x.overlap,
-        l."Hylak_id"  
-            AS hylak_id,
-        l."Lake_name" 
-            AS lake_name
-    FROM fire_area_canada_usa_composite{{YEAR}} 
-        AS x
-    JOIN wbd_hu12 
-        AS ws
-            ON ws.huc12 = x.huc12
-    JOIN lakes_points 
-        AS lp
-            ON ST_Intersects(ST_Transform(ws.geom, 4326), lp.geometry)
-    JOIN lakes 
-        AS l
-            ON l."Hylak_id" = lp."Hylak_id"
-    WHERE x.overlap
+        t.id,
+        t.tnmid,
+        t.globalid,
+        t.huc12,
+        t.name,
+        t.overlap_percentage,
+        l.id   AS hylak_id,
+        l.name AS lake_name
+    FROM fire_area_canada_usa_composite{{YEAR}} AS t
+    JOIN wbd_hu12     AS ws ON ws.huc12 = t.huc12
+    JOIN lakes_points AS lp ON ST_Intersects(ST_Transform(ws.geom, 4326), lp.geom)
+    JOIN lakes        AS l  ON l.id = lp.id
+    WHERE t.overlap
     ORDER BY 
-        x.id, 
-        l."Hylak_id"
+        t.id, 
+        l.id
 ) TO STDOUT WITH CSV HEADER;

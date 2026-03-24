6544d71db661fc0797fd738dcb0d8be0ed0ffca7
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-03-23 18:53:28 -0700

    Refactor union over watershed

--- scripts/fire_union_over_watersheds_polys/q.false.sql
+++ scripts/fire_union_over_watersheds_polys/q.false.sql
@@ -1,41 +1,22 @@
 COPY (
-WITH ws_filtered AS (
+    WITH clipped_watersheds AS (
+        SELECT
+            ws.id                              AS id,
+            ws.tnmid                           AS tnmid,
+            ws.globalid                        AS globalid,
+            ws.huc12                           AS huc12,
+            ws.name                            AS name
+        FROM      wbd_hu12          AS ws
+        JOIN      fire_polys_unions AS fpu ON fpu.year = {{YEAR}}
+        WHERE NOT ST_Intersects(ws.geom, fpu.geom)
+    )
     SELECT
-        ws.id
-            AS id,
-        ws.tnmid
-            AS tnmid,
-        ws.globalid
-            AS globalid,
-        ws.huc12
-            AS huc12,
-        ws.name
-            AS name,
-        ws.geom
-            AS ws_geometry,
-        fu.geometry 
-            AS fu_geometry
-    FROM 
-        wbd_hu12 
-            AS ws
-    JOIN 
-        public.fire_area_canada_usa_unions 
-            AS fu 
-                ON fu.year = {{YEAR}}
-    WHERE NOT ST_Intersects(ws.geom, fu.geometry)
-)
-SELECT
-    id,
-    tnmid,
-    globalid,
-    huc12,
-    name,
-    0 
-        AS overlap_percentage,
-    FALSE 
-        AS overlap
-FROM 
-    ws_filtered
-ORDER BY 
-    id
+        id,
+        tnmid,
+        globalid,
+        huc12,
+        name,
+        0 AS overlap_percentage
+    FROM     clipped_watersheds
+    ORDER BY id
 ) TO STDOUT WITH CSV HEADER;
--- scripts/fire_union_over_watersheds_polys/q.true.sql
+++ scripts/fire_union_over_watersheds_polys/q.true.sql
@@ -1,44 +1,24 @@
 COPY (
-WITH ws_filtered AS (
+    WITH clipped_watersheds AS (
+        SELECT
+            ws.id                              AS id,
+            ws.tnmid                           AS tnmid,
+            ws.globalid                        AS globalid,
+            ws.huc12                           AS huc12,
+            ws.name                            AS name,
+            ST_Intersection(ws.geom, fpu.geom) AS intersection_geometry,
+            ST_Area(ws.geom)                   AS area
+        FROM  wbd_hu12          AS ws
+        JOIN  fire_polys_unions AS fpu ON fpu.year = {{YEAR}}
+        WHERE ST_Intersects(ws.geom, fpu.geom)
+    )
     SELECT
-        ws.id
-            AS id,
-        ws.tnmid
-            AS tnmid,
-        ws.globalid
-            AS globalid,
-        ws.huc12
-            AS huc12,
-        ws.name
-            AS name,
-        ws.geom
-            AS ws_geometry,
-        fu.geometry 
-            AS fu_geometry,
-        ST_Intersection(ws.geom, fu.geometry) 
-            AS intersection_geometry,
-        ST_Area(ws.geom) AS area
-    FROM 
-        wbd_hu12 
-            AS ws
-    JOIN 
-        public.fire_area_canada_usa_unions 
-            AS fu 
-                ON fu.year = {{YEAR}}
-    WHERE ST_Intersects(ws.geom, fu.geometry)
-)
-SELECT
-    id,
-    tnmid,
-    globalid,
-    huc12,
-    name,
-    ST_Area(intersection_geometry) / NULLIF(area, 0) 
-        AS overlap_percentage,
-    (ST_Area(intersection_geometry) / NULLIF(area, 0) > 0) 
-        AS overlap
-FROM 
-    ws_filtered
-ORDER BY 
-    overlap_percentage DESC
+        id,
+        tnmid,
+        globalid,
+        huc12,
+        name,
+        ST_Area(intersection_geometry) / NULLIF(area, 0) AS overlap_percentage
+    FROM     clipped_watersheds
+    ORDER BY overlap_percentage DESC
 ) TO STDOUT WITH CSV HEADER;

cf616ba4020b5be10896cb047a17fe66f5dd6e11
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-03-23 19:52:06 -0700

    Update q.sh

--- scripts/fire_union_over_watersheds_polys/with_lakes/q.sh
+++ scripts/fire_union_over_watersheds_polys/with_lakes/q.sh
@@ -3,7 +3,6 @@
 for year in $(seq 2024 -1 1984); do
     echo "Processing year $year …"
 
-    # Script 1: compute lakes with overlap
     sed "s/{{YEAR}}/$year/g" q.sql | psql -d spatial -o "fire_area_canada_usa_composite${year}.csv"
 
     echo "Completed year $year."

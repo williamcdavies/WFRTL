7744925bfe15929b46a6c792988cc7708aa5549a
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-04-24 11:27:43 -0700

    Organise repo
    Files renamed to match BOX entries. Redundant files removed.

--- scripts/fire_union_over_watersheds_polys/with_lakes/q.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for year in $(seq 2024 -1 1984); do
-    echo "Processing year $year …"
-
-    sed "s/{{YEAR}}/$year/g" q.sql | psql -d spatial -o "fire_area_canada_usa_composite${year}.csv"
-
-    echo "Completed year $year."
-done
--- scripts/hybas/create_table_hybas.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE hybas(
-    id       BIGINT,
-    hybas_id BIGINT,
-    pfaf_id  BIGINT,
-    src_rgn  CHAR(2),
-    geom     geometry(MultiPolygon, 4326),
-    PRIMARY KEY (id)
-);
